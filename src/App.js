907303fa6b7caa7ee2a1d11ed52131b73c203539
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2023-10-18 18:33:31 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
+// function Header() {
+// 	return (
+// 		<div>
+// 			<h1>
+// 				Ali Hadizadeh Moghadam
+// 			</h1>
+// 			<h3>
+// 				Computer Science MSc student @ Sharif University of Technology
+// 			</h3>
+// 		</div>
+// 	);
+// }
+
+// import React from 'react';
+
+const MAIN_COLOR = '#dd5500'
+const MAIN_BLACK = '#400000'
+
 function Header() {
+	const headerStyle = {
+		// whiteSpace: 'nowrap',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: MAIN_COLOR, // Background color for the header
+		color: MAIN_BLACK, // Text color
+		textAlign: 'center', // Center-align text within the header
+		height: '20vmin',
+	};
+
+	const nameStyle = {
+		fontSize: '7vmin',
+		fontWeight: 'bold',
+		fontFamily: 'Noto Sans, sans-serif'
+	};
+
+	const affilStyle = {
+		fontSize: '2.85vmin',
+		fontFamily: 'Noto Sans, sans-serif'
+	};
+
+	const menuButtonStyle = {
+		// display: 'inline-block',
+		textAlign: 'center',
+		fontSize: '10vmin',
+	};
+
+	const titleStyle = {
+		// display: 'inline-block',
+		textAlign: 'center',
+		padding: '1em 0', // Add some padding to the top and bottom of the header
+	};
+
 	return (
-		<div>
-			<h1>
-				Ali Hadizadeh Moghadam
-			</h1>
-			<h3>
-				Computer Science MSc student @ Sharif University of Technology
-			</h3>
-		</div>
+		<header style={headerStyle}>
+				<div style={menuButtonStyle}>
+					X
+				</div>
+				<div style={titleStyle}>
+					<div style={nameStyle}>Ali Hadizadeh Moghadam</div>
+					<div style={affilStyle}><b>Computer Science MSc</b> student <b>@ Sharif</b> University of Technology</div>
+				</div>
+				<div style={menuButtonStyle}>
+					X
+				</div>
+		</header>
 	);
 }
 
-export default function MyApp() {
-	return (
-		<div>
-			<Header />
-		</div>
-	);
-}
+export default Header;
