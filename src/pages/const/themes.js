58ccc29f7a532afea7328a1d9d5165513c96bcd2
Ali Hadizadeh Moghadam <hadizadeh.ali@gmail.com>
2025-08-31 20:54:37 +0330

    Test

---
+++ src/pages/const/themes.js
@@ -0,0 +1,83 @@
+export const themes = {
+	home: {
+		choice: 1,
+		subtheme: [
+			{// 0
+				palette: [
+					"#EEE2DE",
+					"#EA906C",
+					"#B31312",
+					"#2B2A4C"
+				],
+			},
+			{// 1
+				palette: [
+					"#EEE2DE",
+					"#B58876",
+					"#913535",
+					"#283148"
+				],
+			},
+			{// 2
+				palette: [
+					"#EEE2DE",
+					"#EA906C",
+					"#913535",
+					"#2B2A4C"
+				],
+			},
+		],
+	},
+	retro: {
+		choice: 0,
+		subtheme: [
+			{// 0
+				palette: [
+					"#E9E3DF",
+					"#FF7A30",
+					"#465C88",
+					"#000000"
+				],
+			},
+		],
+	},
+	book: {
+		choice: 0,
+		subtheme: [
+			{// 0
+				palette: [
+					"#E3CCAE",
+					"#B8621B",
+					"#262A56",
+					"#000000"
+				],
+			},
+		],
+	},
+	social: {
+		choice: 0,
+		subtheme: [
+			{// 0
+				palette: [
+					"#DCD7C9",
+					"#A27B5C",
+					"#3F4F44",
+					"#2C3930"
+				],
+			},
+		],
+	},
+	dead: {
+		choice: 0,
+		subtheme: [
+			{// 0
+				palette: [
+					"#DDE6ED",
+					"#9DB2BF",
+					"#526D82",
+					"#27374D"
+				],
+			},
+		],
+	},
+}
